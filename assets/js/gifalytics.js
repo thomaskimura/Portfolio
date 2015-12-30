44ad1e6e8fca7f5111e7c4a64180c7fae30386df
thomaskimura <thomas.kimura@ymail.com>
2015-12-30 16:39:27 -0500

    Update work pages 📖

--- assets/js/gifalytics.js
+++ assets/js/gifalytics.js
@@ -7,7 +7,30 @@ function newGif(){
   $.ajax({
     url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=hacking",
     success: function(result){
-      $("header").css("background", "linear-gradient(#d5eeff 15%, rgba(213, 238, 255, 0.5)), url('" + result.data.image_url + "')");
+      $("header").css("background", "linear-gradient(#d5eeff 1%, rgba(213, 238, 255, 0.5)), url('" + result.data.image_url + "')");
     }
   });
 }
+
+$(document).ready(function(){
+  $('.slider').slick({
+    centerMode: true,
+    dots: true,
+    centerPadding: '100px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: '60px'
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerPadding: '30px'
+        }
+      }
+    ]
+  });
+});
